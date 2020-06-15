96d6c2bbec6b73c80b3ad0c247e84a25fb688595
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2020-06-15 21:48:47 +0530

    Update Edit_Distance.py

--- Edit_Distance.py
+++ Edit_Distance.py
@@ -14,9 +14,9 @@ for i in range(m+1):
     for j in range(n+1):
         if(i==0):
             c[i][j]=j
-        if(j==0):
+        elif(j==0):
             c[i][j]=i
-        if(a[i-1]==b[j-1]):
+        elif(a[i-1]==b[j-1]):
             c[i][j]=c[i-1][j-1]
         else:
             c[i][j]=1+min(c[i][j-1],c[i-1][j],c[i-1][j-1])
