2a615a256e57275cf8b6684542f68cb956e57219
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2020-06-16 14:02:03 +0530

    Update 01_Knapsack_Problem.py

--- 01_Knapsack_Problem.py
+++ 01_Knapsack_Problem.py
@@ -11,11 +11,16 @@ val=[0,2,3,4,1]
 n=4
 w=8
 a=[[0]*(w+1) for i in range(5)]
-for i in range(0,n):
-    a[i][0]=0
-    for j  in range(0,w):
-        a[0][j]=0
-        a[i][j]=max(a[i-1][j],a[i-1][j-wt[i]]+val[i])
+for i in range(n+1):
+    for j  in range(w+1):
+        if(i==0):
+            a[i][j]=0
+        elif(j==0):
+            a[i][j]=0
+        elif(wt[i]>j):
+            a[i][j]=a[i-1][j]
+        else:
+            a[i][j]=max(a[i-1][j],a[i-1][j-wt[i]]+val[i])
         
 print(a)
         
