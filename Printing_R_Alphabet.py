a756c78f018ec79a3a0aead95564dd73f620ceec
Manohar Vemuri <29159385+mvemuri6642@users.noreply.github.com>
2020-08-20 19:22:25 +0530

    Update Printing_R_Alphabet.py

--- Printing_R_Alphabet.py
+++ Printing_R_Alphabet.py
@@ -5,7 +5,7 @@ Created on Thu Aug 20 15:36:43 2020
 @author: tejav
 """
 
-
+print()
 for i in range(7):
     for j in range(5):
         if j==0 or (j==4 and (i!=0 and i!=3)) or ((i==0 or i==3) and (j>0 and j<4)):
